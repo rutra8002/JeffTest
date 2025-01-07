82e019dc8d16983b60603b498c584d8234e1d6fa
ruter <arturkummer08@gmail.com>
2025-01-07 22:04:16 +0100

    simple square thing

--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -1,15 +1,67 @@
-#include "raymob.h"
+#include "raylib.h"
+#include "raymath.h"
+
+struct Joystick {
+    Vector2 position;
+    Vector2 origin;
+    float radius;
+    Vector2 direction;
+    bool active;
+};
+
+struct Square {
+    Vector2 position;
+    float size;
+};
+
 int main()
 {
-    InitWindow(0, 0, "raylib [core] example - basic window");
+    InitWindow(0, 0, "Movable Square with Joystick");
+
+    int screenWidth = GetScreenWidth();
+    int screenHeight = GetScreenHeight();
+
+    float joystickRadius = screenHeight * 0.2f;
+    Joystick joystick = { {joystickRadius, static_cast<float>(screenHeight) - joystickRadius}, {joystickRadius, static_cast<float>(screenHeight) - joystickRadius}, joystickRadius, {0, 0}, false };
+    Square square = { {static_cast<float>(screenWidth) / 2, static_cast<float>(screenHeight) / 2}, screenHeight * 0.1f };
+
     SetTargetFPS(60);
+
     while (!WindowShouldClose())
     {
+        DrawFPS(joystickRadius, joystickRadius);
+        if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
+            Vector2 touchPosition = GetMousePosition();
+            if (CheckCollisionPointCircle(touchPosition, joystick.origin, joystick.radius)) {
+                joystick.active = true;
+                joystick.position = touchPosition;
+                joystick.direction = Vector2Subtract(joystick.position, joystick.origin);
+                joystick.direction = Vector2Normalize(joystick.direction);
+            }
+        } else {
+            joystick.active = false;
+            joystick.position = joystick.origin;
+            joystick.direction = {0, 0};
+        }
+
+        // Update square position
+        if (joystick.active) {
+            square.position.x += joystick.direction.x * 5.0f;
+            square.position.y += joystick.direction.y * 5.0f;
+        }
+
         BeginDrawing();
-        ClearBackground(BLACK);
-        DrawText("Hello World", 190, 200, 60, RED);
+        ClearBackground(RAYWHITE);
+
+        DrawCircleV(joystick.origin, joystick.radius, LIGHTGRAY);
+        DrawCircleV(joystick.position, joystick.radius / 2, DARKGRAY);
+
+        DrawRectangleV(square.position, {square.size, square.size}, RED);
+
         EndDrawing();
     }
+
     CloseWindow();
+
     return 0;
 }
