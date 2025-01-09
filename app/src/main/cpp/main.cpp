b6ccf81245bafd3ae746c65e6c4e527261e81467
ruter <arturkummer08@gmail.com>
2025-01-09 15:16:29 +0100

    camera

--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -9,9 +9,18 @@ int main()
     int screenWidth = GetScreenWidth();
     int screenHeight = GetScreenHeight();
 
+    const int baseScreenWidth = 1920;
+    const int baseScreenHeight = 1080;
+
     float joystickRadius = screenHeight * 0.2f;
     Joystick joystick({joystickRadius, static_cast<float>(screenHeight) - joystickRadius}, joystickRadius);
     Player square({static_cast<float>(screenWidth) / 2, static_cast<float>(screenHeight) / 2}, screenHeight * 0.1f);
+
+    Camera2D camera = {0};
+    camera.target = square.position;
+    camera.offset = {static_cast<float>(screenWidth) / 2, static_cast<float>(screenHeight) / 2};
+    camera.rotation = 0.0f;
+    camera.zoom = static_cast<float>(screenHeight) / baseScreenHeight;
 
     while (!WindowShouldClose())
     {
@@ -20,11 +29,17 @@ int main()
         joystick.Update();
         square.Update(joystick, deltaTime);
 
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
         joystick.Draw();
+
+        BeginMode2D(camera);
+
         square.Draw();
+
+        EndMode2D();
 
         DrawFPS(joystickRadius, joystickRadius);
 
